3a491fa1533ee7b53ab8ddbe2f5038c7b19577b3
Paul Smith <paulcsmith0218@gmail.com>
2017-05-27 09:22:34 -0400

    Add initial spec

--- spec/habitat_spec.cr
+++ spec/habitat_spec.cr
@@ -1,9 +1,24 @@
 require "./spec_helper"
 
-describe Habitat do
-  # TODO: Write tests
+private class FakeServer
+  Habitat.create do
+    setting port : Int32
+    setting debug_errors : Bool = true
+  end
 
+  def available_in_instance_methods
+    settings.port
+  end
+end
+
+describe Habitat do
   it "works" do
-    false.should eq(true)
+    FakeServer.configure do
+      settings.port = 8080
+    end
+
+    FakeServer.settings.port.should eq 8080
+    FakeServer.settings.debug_errors.should eq false
+    FakeServer.new.available_in_instance_methods.should eq 8080
   end
 end
