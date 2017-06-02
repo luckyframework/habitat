6b5afc508c2e210ff764b5f30fe0e8c267c39eca
Paul Smith <paulcsmith0218@gmail.com>
2017-06-02 01:00:28 -0400

    Check for missing specs

--- spec/habitat_spec.cr
+++ spec/habitat_spec.cr
@@ -1,8 +1,9 @@
 require "./spec_helper"
 
-private class FakeServer
+class FakeServer
   Habitat.create do
     setting port : Int32
+    setting this_is_missing : String
     setting debug_errors : Bool = true
   end
 
@@ -21,5 +22,16 @@ describe Habitat do
     FakeServer.settings.port.should eq 8080
     FakeServer.settings.debug_errors.should eq true
     FakeServer.new.available_in_instance_methods.should eq 8080
+  end
+
+  it "can check for missing settings" do
+    # Because this_is_missing was never set
+    Habitat.missing_settings?.should be_true
+
+    FakeServer.configure do
+      settings.this_is_missing = "Not anymore"
+    end
+
+    Habitat.missing_settings?.should be_false
   end
 end
--- src/habitat.cr
+++ src/habitat.cr
@@ -1,6 +1,17 @@
 require "./habitat/*"
 
 class Habitat
+  REQUIRED_SETTINGS = [] of String
+
+  macro finished
+    def self.missing_settings?
+      {% for setting in REQUIRED_SETTINGS %}
+        return true if {{ setting.id }}.nil?
+      {% end %}
+      false
+    end
+  end
+
   macro create
     include Habitat::SettingHelpers
 
@@ -26,6 +37,7 @@ class Habitat
     macro setting(decl)
       class Settings
         @@{{ decl.var }} : {{decl.type}} | Nil {% if decl.value %} = {{ decl.value }}{% end %}
+        {% Habitat::REQUIRED_SETTINGS << "#{@type}.settings.#{decl.var}?" %}
 
         def self.{{ decl.var }}=(value : {{ decl.type }})
           @@{{ decl.var }} = value
@@ -33,6 +45,10 @@ class Habitat
 
         def self.{{ decl.var }}
           @@{{ decl.var }}.not_nil!
+        end
+
+        def self.{{ decl.var }}?
+          @@{{ decl.var }}
         end
       end
     end
