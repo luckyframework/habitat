fcd3e91aadafaeba3db34c99f8142cba5d60a90b
Paul Smith <paulcsmith0218@gmail.com>
2018-02-02 06:13:17 -0500

    Set default with boolean = false
    Closes #14

--- spec/habitat_spec.cr
+++ spec/habitat_spec.cr
@@ -5,6 +5,7 @@ class FakeServer
     setting port : Int32
     setting this_is_missing : String
     setting debug_errors : Bool = true
+    setting boolean : Bool = false
   end
 
   def available_in_instance_methods
@@ -14,17 +15,18 @@ end
 
 describe Habitat do
   it "works" do
-    FakeServer.configure do
-      settings.port = 8080
-    end
+    FakeServer.configure { settings.port = 8080 }
 
     typeof(FakeServer.settings.port).should eq Int32
     FakeServer.settings.port.should eq 8080
     FakeServer.settings.debug_errors.should eq true
+    FakeServer.settings.boolean.should eq false
     FakeServer.new.available_in_instance_methods.should eq 8080
   end
 
   it "can check for missing settings" do
+    FakeServer.configure { settings.port = 8080 }
+
     expect_raises(Habitat::MissingSettingError, "this_is_missing") do
       Habitat.raise_if_missing_settings!
     end
