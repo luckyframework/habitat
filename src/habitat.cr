b57f82177e4ddeeb5311f23c03f8d91e8b2a1069
Paul Smith <paulcsmith0218@gmail.com>
2018-10-11 21:46:39 -0400

    Add documentation for temp_config

--- src/habitat.cr
+++ src/habitat.cr
@@ -1,6 +1,7 @@
 require "./habitat/*"
 
 class Habitat
+  # :nodoc:
   class MissingSettingError < Exception
     def initialize(type, setting_name)
       super <<-ERROR
@@ -154,6 +155,7 @@ class Habitat
     end
   end
 
+  # :nodoc:
   module SettingsHelpers
     macro setting(decl)
       {% HABITAT_SETTINGS << decl %}
@@ -207,6 +209,28 @@ class Habitat
   end
 
   module TempConfig
+    # Temporarily changes the configuration
+    #
+    # This method will change the configuration to the passed in value for the
+    # duration of the block. When the block is finished running, Habitat will
+    # then reset to the value before the block
+    #
+    # ```
+    # MyServer.configure do |settings|
+    #   settings.port = 80
+    # end
+    #
+    # MyServer.settings.port # 80
+    #
+    # MyServer.temp_config(port: 3000) do
+    #   MyServer.settings.port # 3000
+    # end
+    #
+    # MyServer.settings.port # 80
+    # ```
+    #
+    # This can be very helpful when writing specs and you need to temporarily
+    # change a value
     macro temp_config(**settings_with_values)
       {% for setting_name, setting_value in settings_with_values %}
         original_{{ setting_name }} = {{ @type.name }}.settings.{{setting_name}}
